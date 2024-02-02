89af6326fcceb65d7836ae73abd1cc3fa864f5fb
Mason-McNeil <masonamc5@gmail.com>
2024-02-01 20:02:51 -0700

    added logic to the server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const express = require("express");
+const sequelizeStore = require("connect-session-sequelize")(
+  expressSession.Store
+);
+const expressSession = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const session = {
+  secret: "", // future secret key
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    // 1 day uptime
+  },
+  store: new sequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`View on https://localhost:${PORT}`);
+  });
+});
