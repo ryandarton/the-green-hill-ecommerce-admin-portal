5221308877d90bb158746f19ba1c28a7199bc8a4
Ryan Darton <ryan@timemachinevideo.com>
2024-02-01 21:49:32 -0700

    Server is working!

--- server.js
+++ server.js
@@ -1,41 +1,14 @@
-const path = require('path');
-const express = require('express');
-const sequelizeStore = require('connect-session-sequelize')(expressSession.Store);
-const expressSession = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-const session = {
-  secret: '', // future secret key
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000,
-    // 1 day uptime
-  },
-  store: new sequelizeStore({
-    db: sequelize,
-  }),
-};
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(expressSession(session));
-
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.set('views', './views');
 
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`View on https://localhost:${PORT}`);
-  });
+app.get('/', (req, res) => {
+  res.render('homepage');
 });
+
+app.listen(3000);
