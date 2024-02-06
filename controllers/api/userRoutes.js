bd47aa2f79b95111dacc86d90aba4cdb53362b3b
Mason-McNeil <masonamc5@gmail.com>
2024-02-05 21:12:55 -0700

    added userRoutes and index for the controllers

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const { Customer } = require("../../models");
+
+router.post("/signup", async (req, res) => {
+  try {
+    const signUpData = await Customer.create(req.body);
+
+    req.session.save(() => {
+      req.session.signup_id = signUpData.id;
+      req.session.signed_up = true;
+      res.json({ customer: signUpData, message: "Thanks for signing up! Your now logged in" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const loginData = await Customer.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (!loginData) {
+      res.status(400).json({ message: "Incorrect login, try again." });
+      return;
+    }
+
+    const validPassword = await loginData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password, try again." });
+    }
+
+    req.session.save(() => {
+      req.session.login_id = loginData.id;
+      req.session.logged_in = true;
+      res.json({ Customer: loginData, message: "Logged In" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+    req.session.destroy(() => {
+      res.json({ message: 'Logged out successfully.' });
+    });
+  });
+
+module.exports = router;
