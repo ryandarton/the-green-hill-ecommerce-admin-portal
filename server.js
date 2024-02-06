60a024098569962fd4399004fcba3b4456dd854d
Ryan Darton <ryan@timemachinevideo.com>
2024-02-05 18:59:55 -0700

    Comment out big block of future code in the server file.

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import { engine } from 'express-handlebars';
 
@@ -15,47 +14,47 @@ app.get('/', (req, res) => {
 app.listen(3000);
 console.log('Server is running on port 3000');
 
-// Future code here:
-const path = require("path");
-const express = require("express");
-const sequelizeStore = require("connect-session-sequelize")(
-  expressSession.Store
-);
-const expressSession = require("express-session");
-const exphbs = require("express-handlebars");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
-const helpers = require("./utils/helpers");
+// Future code here
+// const path = require("path");
+// const express = require("express");
+// const sequelizeStore = require("connect-session-sequelize")(
+//   expressSession.Store
+// );
+// const expressSession = require("express-session");
+// const exphbs = require("express-handlebars");
+// const routes = require("./controllers");
+// const sequelize = require("./config/connection");
+// const helpers = require("./utils/helpers");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-const session = {
-  secret: "", // future secret key
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000,
-    // 1 day uptime
-  },
-  store: new sequelizeStore({
-    db: sequelize,
-  }),
-};
+// const session = {
+//   secret: "", // future secret key
+//   cookie: {
+//     maxAge: 24 * 60 * 60 * 1000,
+//     // 1 day uptime
+//   },
+//   store: new sequelizeStore({
+//     db: sequelize,
+//   }),
+// };
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use(expressSession(session));
+// app.use(expressSession(session));
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
 
-app.use(routes);
+// app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`View on https://localhost:${PORT}`);
-  });
-});
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`View on https://localhost:${PORT}`);
+//   });
+// });
