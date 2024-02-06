f5a7a9064f106e01ed0ad0efa8cc2852889dcb9f
Ryan Darton <ryan@timemachinevideo.com>
2024-02-05 21:10:50 -0700

    Fix server, once again!

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
+import path from 'path';
+import url from 'url';
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename);
+console.log(__dirname);
 const app = express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('homepage');
