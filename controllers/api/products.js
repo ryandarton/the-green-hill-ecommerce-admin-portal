9341d8cc53ed9ea61f3b61dc5ecf4ec245183d6e
maglb <magali.lb21@gmail.com>
2024-02-05 20:14:23 -0700

    Product route created

---
+++ controllers/api/products.js
@@ -0,0 +1,82 @@
+const router = require("express").Router();
+const { Customer, Order, Product } = require("../../models");
+
+// get all products
+router.get("/", async (req, res) => {
+
+  try {
+    const productsData = await Product.findAll();
+    res.status(200).json(productsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one product
+router.get("/:id", async (req, res) => {
+
+  try {
+    const productData = await Product.findByPk(req.params.id);
+
+    if (!productData) {
+      res.status(404).json({ message: "No product was found with that id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create new product
+router.post("/", (req, res) => {
+
+  Product.create(req.body)
+    .then((product) => {
+      res.status(200).json(product);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+// update product
+router.put("/:id", async (req, res) => {
+
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+    res.json(product);
+    })
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete("/:id", async (req, res) => {
+
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product was found with that id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
