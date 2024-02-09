8ea5f7951429d804775a76851b9a8ba3a10dacbc
Mason-McNeil <masonamc5@gmail.com>
2024-02-08 23:10:49 -0700

    new files

---
+++ models/Admin.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt')
+
+class Admin extends Model {
+  async checkPassword(password) {
+    return await bcrypt.compare(password, this.password);
+  }
+}
+
+Admin.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 20],
+      },
+    },
+  
+  },
+  {
+    sequelize,
+    hooks: {
+      async beforeCreate(newAdminData) {
+        newAdminData.password = await bcrypt.hash(newAdminData.password, 10);
+        return newAdminData;
+      },
+    },
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'admin',
+  }
+),
+  module.exports = Admin;
---
+++ models/Product.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    size: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    subscribable: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
