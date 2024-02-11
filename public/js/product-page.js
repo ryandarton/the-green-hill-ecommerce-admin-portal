1df3d6db005ab3504c962fac6da8ea7e005a9f4a
Ryan Darton <ryan@timemachinevideo.com>
2024-02-11 16:59:22 -0700

    JS logic to dismiss the mobile message.

--- public/js/product-page.js
+++ public/js/product-page.js
@@ -1,16 +1,16 @@
-const deleteBtn = document.getElementById("delete-btn");
-const editBtn = document.getElementById("edit-btn");
+const deleteBtn = document.getElementById('delete-btn');
+const editBtn = document.getElementById('edit-btn');
 
 const deleteProduct = (id) => {
   fetch(`/api/products/${id}`, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
   });
 };
 
-deleteBtn.addEventListener("click", function (e) {
+deleteBtn.addEventListener('click', function (e) {
   e.stopPropagation();
-  console.log("clicked");
+  console.log('clicked');
   const productId = e.target.dataset.id;
   console.log(e.target);
   console.log(e.target.dataset);
@@ -19,48 +19,51 @@ deleteBtn.addEventListener("click", function (e) {
   location.reload();
 });
 
-$(".save").hide();
+$('.save').hide();
 
 // EDIT FUNCTION
-$(document).on("click", ".edit", function () {
-  $(this).parent().siblings("td.data").each(function () {
+$(document).on('click', '.edit', function () {
+  $(this)
+    .parent()
+    .siblings('td.data')
+    .each(function () {
       var content = $(this).html();
       // console.log(content);
       $(this).html('<input value="' + content + '" />');
       // console.log(this);
     });
 
-  $(this).siblings(".save").show();
-  $(this).siblings("#delete-btn").hide();
+  $(this).siblings('.save').show();
+  $(this).siblings('#delete-btn').hide();
   $(this).hide();
 });
 
 const updateProduct = (id, data) => {
   fetch(`/api/products/${id}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   });
 };
 
-$(document).on("click", ".save", function () {
-  $("input").each(function () {
+$(document).on('click', '.save', function () {
+  $('input').each(function () {
     var content = $(this).val();
     $(this).html(content);
     $(this).contents().unwrap();
   });
-  $(this).siblings(".edit").show();
-  $(this).siblings("#delete-btn").show();
+  $(this).siblings('.edit').show();
+  $(this).siblings('#delete-btn').show();
   $(this).hide();
 
-  const productId = $(this).parent().siblings(".id").html();
+  const productId = $(this).parent().siblings('.id').html();
   console.log(productId);
 
-const product_name = $(this).parent().siblings(".name").html();
-const product_weight = $(this).parent().siblings(".weight").html();
-const product_size = $(this).parent().siblings(".size").html();
-const product_price = $(this).parent().siblings(".price").html();
-const product_quantity = $(this).parent().siblings(".quantity").html();
+  const product_name = $(this).parent().siblings('.name').html();
+  const product_weight = $(this).parent().siblings('.weight').html();
+  const product_size = $(this).parent().siblings('.size').html();
+  const product_price = $(this).parent().siblings('.price').html();
+  const product_quantity = $(this).parent().siblings('.quantity').html();
 
   const updatedProduct = {
     name: product_name,
@@ -72,5 +75,10 @@ const product_quantity = $(this).parent().siblings(".quantity").html();
 
   console.log(updatedProduct);
 
-updateProduct(productId, updatedProduct);
+  updateProduct(productId, updatedProduct);
+});
+
+// When the button with the id="close-message" is clicked, the display of the element with the class="message" is set to "none"
+$('#close-message').on('click', function () {
+  $('.message').css('display', 'none');
 });
