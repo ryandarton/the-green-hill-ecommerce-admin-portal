01c7adbe3df1472dff53a2e37cfe0fa58f6910dd
maglb <magali.lb21@gmail.com>
2024-02-11 11:46:49 -0700

    editable row function added

--- public/js/product-page.js
+++ public/js/product-page.js
@@ -19,82 +19,47 @@ deleteBtn.addEventListener("click", function (e) {
   location.reload();
 });
 
+$(".save").hide();
 
-//WORKING ON EDIT FUNCTION
-// const editProduct = (id) => {
-//   fetch(`/api/products/${id}`, {
-//     method: "PUT",
-//     headers: { "Content-Type": "application/json" },
-//   });
-// };
+// EDIT FUNCTION
+$(document).on("click", ".edit", function () {
+  $(this).parent().siblings("td.data").each(function () {
+      var content = $(this).html();
+      // console.log(content);
+      $(this).html('<input value="' + content + '" />');
+      // console.log(this);
+    });
 
-// editBtn.addEventListener("click", function (e) {
-//   e.stopPropagation();
-//   console.log("clicked");
-//   const productId = e.target.dataset.id;
-//   console.log(e.target);
-//   console.log(e.target.dataset);
-//   console.log(productId);
-//   deleteProduct(productId);
-//   location.reload();
+  $(this).siblings(".save").show();
+  $(this).siblings("#delete-btn").hide();
+  $(this).hide();
+});
+
+$(document).on("click", ".save", function () {
+  $("input").each(function () {
+    var content = $(this).val();
+    $(this).html(content);
+    // console.log(content);
+    $(this).contents().unwrap();
+  });
+  $(this).siblings(".edit").show();
+  $(this).siblings("#delete-btn").show();
+  $(this).hide();
+
+
+
+
+  
+});
+
+// $(document).on("click", ".delete", function () {
+//   $(this).parents("tr").remove();
 // });
 
-
-// //Edit: WORKING ON 
-
-// function Edit(element) {
-
-//   const row = element.parentNode.parentNode;
-//   const tdList = row.children;
-
-//   for (let index = 0; index < tdList.length - 1; index++) {
-//     const element = tdList[index];
-//     var str = element.childNodes[0].nodeValue;
-//     var input = document.createElement("input");
-//     input.type = "text";
-//     input.id = "edit" + (index + 1).toString();
-//     input.value = str;
-//     element.removeChild(element.childNodes[0]);
-//     element.appendChild(input);
-
-
-
-//   }
-//   const tdBtns = tdList[1];
-//   const btns = tdBtns.children;
-//   deleteButtons(btns, tdBtns);
-//   createButtons(false, tdBtns);
-// }
-
-
-// function Save(element) {
-//   const row = element.parentNode.parentNode;
-//   const tdList = row.children;
-
-//   /* const edit = [
-//       ['edit1'],
-//       ['edit2']
-//   ]; */
-
-//   const edit = [];
-//   for (let index = 0; index <= 1; index++) {
-//     edit[index] = document.getElementById("edit" + (index + 1).toString()).value;
-//     if (edit[index] == "") {
-//       alert("You must not keep textboxes empty");
-//       var empty = true;
-//     }
-//   }
-
-//   if (!empty) {
-//     for (let index = 0; index < tdList.length - 1; index++) {
-//       tdList[index].removeChild(tdList[index].children[0]);
-//       var text = document.createTextNode(edit[index]);
-//       tdList[index].appendChild(text);
-//     }
-//     const tdBtns = tdList[1];
-//     const btns = tdBtns.children;
-//     deleteButtons(btns, tdBtns);
-//     createButtons(true, tdBtns);
-
-//   }
-// }
+// $(".add").click(function () {
+//   $(this)
+//     .parents("table")
+//     .append(
+//       '<tr><td class="data"></td><td class="data"></td><td class="data"></td><td><button class="save">Save</button><button class="edit">Edit</button> <button class="delete">Delete</button></td></tr>'
+//     );
+// });
