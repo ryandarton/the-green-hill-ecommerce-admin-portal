34d050254eb388d5cacd080c2ddf7de5bbd082fc
Ryan Darton <ryan@timemachinevideo.com>
2024-02-12 02:05:57 -0700

    Attempt to solve heroku error.

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/', (req, res) => {
 });
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
   });
 });
